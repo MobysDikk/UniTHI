7b341c767dbcebc349d4eabe8f842f635f277988
Moris Breitenborn <moris1992@gmx.de>
2019-10-21 18:00:04 +0200

    SE Programm Qualität verbessern

---
+++ Semester3/SoftwareEngineering/version0/src/version0/FileReciver.java
@@ -0,0 +1,39 @@
+package version0;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This class is the entry point in the compare application.
+ * it will check if there are exactly two arguments given. 
+ * 
+ *
+ */
+
+public class FileReciver{
+     
+    private static final Logger log = Logger.getLogger( FileReciver.class.getName() );
+
+	public static void main( String [] args ) throws IOException, InterruptedException {
+	    
+	    if( args.length != 2 ) {
+	        FileHandler handler = new FileHandler( "log.txt" );
+	        handler.setLevel( Level.ALL );
+	        log.addHandler( handler );
+	        log.info("More or less than 2 arguments entered");
+            return;        
+	    }
+
+		FileComperator prototyp = new FileComperator( new File( args[ 0 ] ),  new File( args[ 1 ] ) );
+
+		while( true ) {
+			if( ! prototyp.compare() ) {
+			    SourceFileCopy.copy( new File( args[ 0 ] ),  new File( args[ 1 ] ) );
+			}
+			Thread.sleep( 10000 );
+		}
+	}
+}
---
+++ Semester3/SoftwareEngineering/version0/src/version0/SourceFileCopy.java
@@ -0,0 +1,33 @@
+package version0;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class SourceFileCopy {
+    /*
+     * This class copy the source File content and 
+     * overwrite the content of the target File
+     * 
+     * 
+     * @param sorceFile
+     * @param targetFile
+     * @throws IOException
+     */
+    
+	public static void copy( File sourceFile, File targetFile ) throws IOException{
+
+		FileInputStream  fIn  = new FileInputStream( sourceFile );
+		FileOutputStream fOut = new FileOutputStream( targetFile );
+
+		byte [] content = new byte[ 4096 ];
+
+		int length = fIn.read( content );
+		fOut.write( content, 0, length );
+
+		fIn.close();
+		fOut.close();
+	}
+
+}
